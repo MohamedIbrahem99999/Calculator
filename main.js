6849a696bdff751b5f5ed04843e49120d2a348b7
Mohamed Ibrahem <mibrahem99999@gmail.com>
2026-07-01 01:45:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-let valueTxt = document.getElementById("value");
-let keys = document.querySelectorAll(".key")
+alert("محمد ابراهيم يرحب بكم");
 
-keys.forEach(
-    key => {
-        key.onclick = function () {
-            if (key.dataset.val === "clear") valueTxt.value = "";
-            else if (key.dataset.val === "equal")
-              valueTxt.value = eval(valueTxt.value);
-            else valueTxt.value += key.dataset.val;
-        };
+let valueTxt = document.getElementById("value");
+let keys = document.querySelectorAll(".btn");
+
+keys.forEach(key => {
+
+    key.addEventListener("click", function () {
+
+        console.log(key.dataset.val);
+
+        if (key.dataset.val === "clear") {
+            valueTxt.value = "";
+        }
+
+        else if (key.dataset.val === "back") {
+            valueTxt.value = valueTxt.value.slice(0, -1);
+        }
+
+        else if (key.dataset.val === "equal") {
+            try {
+                valueTxt.value = eval(valueTxt.value);
+            } catch {
+                valueTxt.value = "Error";
+            }
+        }
+
+        else {
+            valueTxt.value += key.dataset.val;
+        }
+
     });
+
+});
